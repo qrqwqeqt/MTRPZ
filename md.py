5ac67682d0775a6a273c498394ca282d337cdf82
qrqwqeqt <qrqwqeqt@gmail.com>
2024-05-04 19:26:30 +0200

    Revert "add usage flag"
    This reverts commit 749cc436755e6019aaf0ecd7efee382e78a96f23.

--- md.py
+++ md.py
@@ -1,5 +1,3 @@
-import argparse
-from colorama import Fore, Back, Style
 import re
 import os
 
@@ -62,26 +60,8 @@ def markdown_to_html(filePath):
 
     return ''.join(parts)
 
-# Створюємо парсер аргументів
-parser = argparse.ArgumentParser(description='Markdown to HTML converter')
-parser.add_argument('--format', type=str, help='Output format')
 
-# Розбираємо аргументи командного рядка
-args = parser.parse_args()
 
-# Визначаємо формат виводу
-output_format = args.format if args.format else 'console'
-
-# Виконуємо перетворення
 html = markdown_to_html('md.md')
-
-# Виводимо результат
-if output_format == 'console':
-    # Виводимо в консоль з використанням ANSI Escape Codes
-    print(Fore.GREEN + Style.BRIGHT + html + Style.RESET_ALL)
-elif output_format == 'file':
-    # Виводимо в файл
-    with open('output.html', 'w', encoding='utf-8') as f:
-        f.write(html)
-else:
-    print(f'Unsupported format: {output_format}')
+with open('output.html', 'w', encoding='utf-8') as f:
+    f.write(html)
